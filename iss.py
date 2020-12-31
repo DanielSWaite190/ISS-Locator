5c156bc8d196676239a9852bf35451101604690b
Daniel S. Waite <danielwaite190@gmail.com>
2020-12-31 10:11:41 -0700

    Corecting indianapolis coordinates

--- iss.py
+++ iss.py
@@ -1,9 +1,5 @@
 #!/usr/bin/env python
 
-# http://api.open-notify.org/astros.json
-# http://api.open-notify.org/iss-now.json
-# http://api.open-notify.org/iss-pass.json
-
 __author__ = 'Daniel S. Waite'
 
 import requests
@@ -22,17 +18,28 @@ def main():
     latitude = float(coordinates.get("latitude"))
     longitude = float(coordinates.get("longitude"))
 
-
     backdrop = turtle.Screen()
     backdrop.bgpic("map.gif")
 
     iss = turtle.Turtle()
+    backdrop.setup(width=720, height=360)
+    backdrop.setworldcoordinates(-180, -90, 180, 90)
     backdrop.register_shape("iss.gif")
     iss.shape("iss.gif")
     iss.penup()
     iss.goto(latitude, longitude)
-    turtle.done()
     
+
+    indianapolis = turtle.Turtle()
+    indianapolis.shape("circle")
+    indianapolis.fillcolor("yellow")
+    indianapolis.penup()
+    indianapolis.goto(-86.158, 39.7684)
+
+    turtle.done()
+
+    # http://api.open-notify.org/iss-pass.json?=36
+    # http://api.open-notify.org/iss-pass.json
 
 if __name__ == '__main__':
     main()
