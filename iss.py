174ed9d4e7063d5eb6e71fa43e42574d74261b66
Daniel S. Waite <danielwaite190@gmail.com>
2021-01-11 11:31:12 -0700

    Fixed api problem and turned off visual map

--- iss.py
+++ iss.py
@@ -17,55 +17,52 @@ def main():
     print()
 
     coordinates = requests.get("http://api.open-notify.org/iss-now.json").json()
-    latitude = float(coordinates.get("iss_position").get("latitude"))
-    longitude = float(coordinates.get("iss_position").get("longitude"))
+    latitude = float(coordinates["iss_position"]["latitude"])
+    longitude = float(coordinates["iss_position"]["longitude"])
 
-    backdrop = turtle.Screen()
-    backdrop.bgpic("map.gif")
+    print(f"Time: {time.ctime(coordinates['timestamp'])}")
+    print(f"ISS latitude: {latitude}")
+    print(f"ISS longitude: {longitude}")
+    # print(f"Passover time: {time.ctime(passover.datetime)}")
+    print()
+
+    # backdrop = turtle.Screen()
+    # backdrop.bgpic("map.gif")
 
     try:
-        p = requests.get("http://api.open-notify.org/iss-pass.json?lat=-86.158&lon=39.7684")
+        p = requests.get("http://api.open-notify.org/iss-pass.json?lat=39.7684&lon=86.158")
         p.raise_for_status()
     except requests.exceptions.HTTPError:
         print("\n\nTHE API IS DOWN\n\n")  
 
-    passover = p.json
-    
-    # passover = {
-    #     "message": "success",
-    #     "request": {
-    #     "latitude": 1234,
-    #     "longitude": 5678, 
-    #     "altitude": 5,
-    #     "passes": 7,
-    #     "datetime": 1545730073
-    #     },
-    #         "response": [{"risetime": 1545730073, "duration": "3 hours"}]
-    #     }
+    passover = p.json()
+    print(f"Next Indianapolis passover time: {time.ctime(passover['request']['datetime'])}")
+    print()
 
-    iss = turtle.Turtle()
-    backdrop.setup(width=720, height=360)
-    backdrop.setworldcoordinates(-180, -90, 180, 90)
-    backdrop.register_shape("iss.gif")
-    iss.shape("iss.gif")
-    iss.penup()
-    iss.goto(latitude, longitude)
-    iss.write(time.ctime(coordinates.get("timestamp")), font=('Courier', 12, 'bold'), align='left')
+
+    # iss = turtle.Turtle()
+    # backdrop.setup(width=720, height=360)
+    # backdrop.setworldcoordinates(-180, -90, 180, 90)
+    # backdrop.register_shape("iss.gif")
+    # iss.shape("iss.gif")
+    # iss.penup()
+    # iss.goto(latitude, longitude)
+    # iss.write(time.ctime(coordinates.get("timestamp")), font=('Courier', 12, 'bold'), align='left')
     
 
-    indianapolis = turtle.Turtle()
-    indianapolis.shape("circle")
-    indianapolis.fillcolor("yellow")
-    indianapolis.penup()
-    indianapolis.goto(-86.158, 39.7684)
+    # indianapolis = turtle.Turtle()
+    # indianapolis.shape("circle")
+    # indianapolis.fillcolor("yellow")
+    # indianapolis.penup()
+    # indianapolis.goto(-86.158, 39.7684)
 
-    indianapolis.color("yellow") # <-- wrong
-    indianapolis.write(time.ctime(passover.get("request").get("datetime")), font=('Courier', 12, 'bold'), align='left')
+    # indianapolis.color("yellow") # <-- wrong
+    # indianapolis.write(time.ctime(passover.get("request").get("datetime")), font=('Courier', 12, 'bold'), align='left')
 
     # print(time.ctime(passover.get("request").get("datetime")))
     # print(time.ctime(coordinates.get("timestamp")))
 
-    turtle.done()
+    # turtle.done()
 
 if __name__ == '__main__':
     main()
